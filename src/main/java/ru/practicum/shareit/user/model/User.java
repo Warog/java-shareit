dfacdbe28a1bf5cdad482795414b08832b48dde8
Shvelidze Enri <ehnrike-shvelidze@yandex.ru>
2023-01-04 11:51:50 +0300

    fix: change a User.Class

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,8 +1,9 @@
 package ru.practicum.shareit.user.model;
 
-import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
@@ -11,11 +12,20 @@ import javax.validation.constraints.NotBlank;
  */
 
 @Data
-@Builder
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
     private Integer id;
+//    @Column(nullable = false, length = 64)
     private String name;
     @Email(message = "Неверно указан email")
     @NotBlank(message = "Неверные данные: Email пуст или содержит только пробелы")
     private String email;
+    @Enumerated(EnumType.STRING)
+    private UserState state;
+
+    public enum UserState {
+        ACTIVE, BLOCKED, DELETED;
+    }
 }
